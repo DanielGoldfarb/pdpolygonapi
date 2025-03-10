4f8d990fc4826fc22545a0892c73169e484e5bcf
Daniel Goldfarb <dgoldfarb.github@gmail.com>
2025-03-09 22:20:44 -0400

    tweak re-golden

--- tests/test_ohlcv_stocks.py
+++ tests/test_ohlcv_stocks.py
@@ -11,8 +11,6 @@ print(f" Using logger({__name__})")
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-DATE_FORMAT = "%Y-%m-%d"
-
 ticker_param_data = [
     # ["ticker", "start", "end", "span", "span_multiplier"],
     ("SPY", "2023-10-01", "2025-03-01", "day", 1),
@@ -40,10 +38,11 @@ def test_ohlcv_stocks(pdpgapi, regolden, ticker, start, end, span, span_multipli
         ref_name = f"reference_data/{ticker}_{sd}_{ed}_{span}_{span_multiplier}.csv"
         if regolden:
             df.to_csv(ref_name)
+            message = f"NOT TESTING: REGENERATING 'golden' reference file: '{ref_name}'"
+            pytest.fail(message)
         else:
             rdf = pd.read_csv(ref_name, index_col=ticker, parse_dates=True)
             # logger.info(f"rdf=\n{rdf.iloc[[0,-1]]}")
             # https://saturncloud.io/blog/how-to-confirm-equality-of-two-pandas-dataframes
             pd.testing.assert_frame_equal(df, rdf)
 
-    logger.info(f"regolden={regolden}  ref_name={ref_name}")
